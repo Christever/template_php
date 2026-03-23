f3ccb66c2960eb75d3ab07c93024b9573797fbd3
Christever <christian@steverlynck.fr>
2026-03-23 17:17:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "public/*.{html,js,php}",
+    "src/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
+
